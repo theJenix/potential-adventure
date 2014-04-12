6c4c0ece0f630764b70e262e64b6faaafd97e79b
Perry Shuman <fortytwofortytwo@gmail.com>
2014-04-12 03:32:36 -0400

    Audio routes and test

--- site/config/routes.rb
+++ site/config/routes.rb
@@ -5,6 +5,8 @@ Site::Application.routes.draw do
 
   resources :users
 
+  match "/audio" => redirect("/audio")
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
